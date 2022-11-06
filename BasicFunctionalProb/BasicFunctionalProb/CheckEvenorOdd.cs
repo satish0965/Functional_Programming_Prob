8dd661fa53a22a3e3fe708e6f868d828744ec6c0
Satish Patil <satish.patil7017@gmail.com>
2022-11-06 15:13:40 +0530

    Added Number is Even or Odd Problem Solution

---
+++ BasicFunctionalProb/BasicFunctionalProb/CheckEvenorOdd.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicFunctionalProb
+{
+    public class CheckEvenorOdd
+    {
+        public void EvenOrOdd()
+        {
+
+            Console.Write("\tEnter the number to Check: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("\t{0} is an Even number", num);
+            }
+            else
+            {
+                Console.WriteLine("\t{0} is an Odd number", num);
+            }
+        }
+    }
+}
