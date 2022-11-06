c2cac421e72150727d6f61be4ab26d1a40decb7d
Satish Patil <satish.patil7017@gmail.com>
2022-11-06 15:22:10 +0530

    Added Vowels in Alphabets Problem Solution

---
+++ BasicFunctionalProb/BasicFunctionalProb/CheckOvels.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicFunctionalProb
+{
+    public class CheckOvels
+    {
+        public void CheckVowelOrConsonant()
+        {
+            Console.Write("\tEnter an Alphabet: ");
+            char letter = Convert.ToChar(Console.ReadLine());
+            if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u')
+            {
+                Console.WriteLine("\t{0}: is a vowel", letter);
+            }
+            else
+            {
+                Console.WriteLine("\t{0}: is a Consonant", letter);
+            }
+        }
+    }
+}
--- BasicFunctionalProb/BasicFunctionalProb/Program.cs
+++ BasicFunctionalProb/BasicFunctionalProb/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿/// See https://aka.ms/new-console-template for more information
 using System;
 
 namespace BasicFunctionalProb
@@ -16,6 +16,9 @@ namespace BasicFunctionalProb
             Console.WriteLine("5 for Factors");
             Console.WriteLine("6 for Quotient and remainder");
             Console.WriteLine("7 for Swap two Number");
+            Console.WriteLine("8 for Check even or odd");
+            Console.WriteLine("9 for Check Vowels");
+
 
             int Problem = Convert.ToInt32(Console.ReadLine());
             switch (Problem)
@@ -47,6 +50,14 @@ namespace BasicFunctionalProb
                 case 7:
                     SwappingNumber swapnumber = new SwappingNumber();
                     swapnumber.SwapTwoNumber();
+                    break;
+                case 8:
+                    CheckEvenorOdd checkevenorodd = new CheckEvenorOdd();
+                    checkevenorodd.EvenOrOdd();
+                    break;
+                case 9:
+                    CheckOvels ovels = new CheckOvels();
+                    ovels.CheckVowelOrConsonant();
                     break;
             }
         }
